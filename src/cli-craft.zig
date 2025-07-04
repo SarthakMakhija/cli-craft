10559818b21e3315ed89a49db1ebe8303f68bb82
Sarthak <sarthak.makhija@gmail.com>
2025-07-04 11:52:40 +0530

    Sarthak | Adds support for adding flags

--- src/cli-craft.zig
+++ src/cli-craft.zig
@@ -7,6 +7,7 @@ const std = @import("std");
 pub const Command = @import("command.zig");
 pub const Commands = @import("commands.zig");
 pub const ArgumentSpecification = @import("argument-specification.zig");
+pub const Flags = @import("flags.zig");
 
 test {
     // Reference all tests from modules
--- src/flags.zig
+++ src/flags.zig
@@ -1,9 +1,50 @@
 const std = @import("std");
 
+pub const Flags = struct {
+    flags: std.StringHashMap(Flag),
+    short_name_to_long_name: std.AutoHashMap(u8, []const u8),
+
+    pub fn init(allocator: std.mem.Allocator) Flags {
+        return .{
+            .flags = std.StringHashMap(Flag).init(allocator),
+            .short_name_to_long_name = std.AutoHashMap(u8, []const u8).init(allocator),
+        };
+    }
+
+    pub fn addFlag(self: *Flags, flag: Flag) !void {
+        try self.flags.put(flag.name, flag);
+        if (flag.short_name) |short_name| {
+            try self.short_name_to_long_name.put(short_name, flag.name);
+        }
+    }
+
+    pub fn contains(self: *Flags, flag_name: []const u8) bool {
+        return self.flags.contains(flag_name) or
+            (flag_name.len > 0 and self.short_name_to_long_name.contains(flag_name[0]));
+    }
+
+    pub fn deinit(self: *Flags) void {
+        self.flags.deinit();
+        self.short_name_to_long_name.deinit();
+    }
+};
+
 pub const FlagValue = union(enum) {
     boolean: bool,
     int64: i64,
     string: []const u8,
+
+    pub fn type_boolean(value: bool) FlagValue {
+        return .{ .boolean = value };
+    }
+
+    pub fn type_int64(value: i64) FlagValue {
+        return .{ .int64 = value };
+    }
+
+    pub fn type_string(value: []const u8) FlagValue {
+        return .{ .string = value };
+    }
 };
 
 pub const Flag = struct {
@@ -79,12 +120,12 @@ test "build a boolean flag with name and description" {
 test "build a boolean flag with short name and default value" {
     const verbose_flag = Flag.builder("verbose", "Enable verbose output")
         .withShortName('v')
-        .withDefaultValue(FlagValue.boolean(false))
+        .withDefaultValue(FlagValue.type_boolean(false))
         .build();
 
     try std.testing.expectEqualStrings("verbose", verbose_flag.name);
     try std.testing.expectEqualStrings("Enable verbose output", verbose_flag.description);
-    try std.testing.expectEqual(false, verbose_flag.default_value.?);
+    try std.testing.expectEqual(false, verbose_flag.default_value.?.boolean);
     try std.testing.expectEqual('v', verbose_flag.short_name.?);
 }
 
@@ -100,14 +141,15 @@ test "build a int64 flag with name and description" {
 
 test "build a int64 flag with short name and default value" {
     const count_flag = Flag.builder("count", "Count items")
-        .withDefaultValue(FlagValue.int64(10))
-        .wothShortName('c')
+        .withDefaultValue(FlagValue.type_int64(10))
+        .withShortName('c')
         .build();
 
     try std.testing.expectEqualStrings("count", count_flag.name);
     try std.testing.expectEqualStrings("Count items", count_flag.description);
-    try std.testing.expectEqual(false, count_flag.default_value.?);
-    try std.testing.expectEqual('v', count_flag.short_name.?);
+
+    try std.testing.expectEqual(10, count_flag.default_value.?.int64);
+    try std.testing.expectEqual('c', count_flag.short_name.?);
 }
 
 test "build a string flag with name and description" {
@@ -123,11 +165,53 @@ test "build a string flag with name and description" {
 test "build a string flag with short name and default value" {
     const namespace_flag = Flag.builder("namespace", "Define the namespace")
         .withShortName('n')
-        .withDefaultValue(FlagValue.string("default_namespace"))
+        .withDefaultValue(FlagValue.type_string("default_namespace"))
         .build();
 
     try std.testing.expectEqualStrings("namespace", namespace_flag.name);
     try std.testing.expectEqualStrings("Define the namespace", namespace_flag.description);
-    try std.testing.expectEqualStrings("default_namespace", namespace_flag.default_value.?);
+    try std.testing.expectEqualStrings("default_namespace", namespace_flag.default_value.?.string);
     try std.testing.expectEqual('n', namespace_flag.short_name.?);
+}
+
+test "add a flag and check its existence by name" {
+    const namespace_flag = Flag.builder("namespace", "Define the namespace")
+        .withShortName('n')
+        .withDefaultValue(FlagValue.type_string("default_namespace"))
+        .build();
+
+    var flags = Flags.init(std.testing.allocator);
+    defer flags.deinit();
+
+    try flags.addFlag(namespace_flag);
+
+    try std.testing.expect(flags.contains("namespace"));
+}
+
+test "add a flag and check its existence by short name" {
+    const namespace_flag = Flag.builder("namespace", "Define the namespace")
+        .withShortName('n')
+        .withDefaultValue(FlagValue.type_string("default_namespace"))
+        .build();
+
+    var flags = Flags.init(std.testing.allocator);
+    defer flags.deinit();
+
+    try flags.addFlag(namespace_flag);
+
+    try std.testing.expect(flags.contains("n"));
+}
+
+test "check the existence of a non-existing flag by name" {
+    var flags = Flags.init(std.testing.allocator);
+    defer flags.deinit();
+
+    try std.testing.expect(flags.contains("verbose") == false);
+}
+
+test "check the existence of a non-existing flag by short name" {
+    var flags = Flags.init(std.testing.allocator);
+    defer flags.deinit();
+
+    try std.testing.expect(flags.contains("v") == false);
 }
