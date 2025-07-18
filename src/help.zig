f79760d959dc9c5e6404a7375159612915aa5212
Sarthak <sarthak.makhija@gmail.com>
2025-07-18 09:36:24 +0530

    Sarthak | Prints usage in command help

--- src/help.zig
+++ src/help.zig
@@ -41,22 +41,25 @@ pub const CommandHelp = struct {
         var usage: std.ArrayList(u8) = std.ArrayList(u8).init(allocator);
         defer usage.deinit();
 
-        try usage.writer().print("Usage: {s}", .{self.command.name});
-
-        switch (self.command.action) {
-            .executable => {
-                if (flags.flag_by_name.count() > 0) {
-                    try usage.writer().writeAll(" [flags]");
-                }
-                try usage.writer().writeAll(" [arguments]");
-            },
-            .subcommands => {
-                try usage.writer().writeAll(" [subcommand]");
-                if (flags.flag_by_name.count() > 0) {
-                    try usage.writer().writeAll(" [flags]");
-                }
-                try usage.writer().writeAll(" [arguments]");
-            },
+        if (self.command.usage) |command_usage| {
+            try usage.writer().print("Usage: {s}", .{command_usage});
+        } else {
+            try usage.writer().print("Usage: {s}", .{self.command.name});
+            switch (self.command.action) {
+                .executable => {
+                    if (flags.flag_by_name.count() > 0) {
+                        try usage.writer().writeAll(" [flags]");
+                    }
+                    try usage.writer().writeAll(" [arguments]");
+                },
+                .subcommands => {
+                    try usage.writer().writeAll(" [subcommand]");
+                    if (flags.flag_by_name.count() > 0) {
+                        try usage.writer().writeAll(" [flags]");
+                    }
+                    try usage.writer().writeAll(" [arguments]");
+                },
+            }
         }
         try self.output_stream.print("{s} \n", .{usage.items});
     }
@@ -191,6 +194,50 @@ test "print command help for a command that has no subcommands" {
 
     try command_help.printHelp(std.testing.allocator, &flags);
 
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "Usage: stringer").? >= 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "stringer").? >= 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "str").? >= 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "strm").? >= 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "verbose").? > 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "priority").? > 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "timeout").? > 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "--help").? > 0);
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "-h").? > 0);
+}
+
+test "print command help for a command with usage defined" {
+    const runnable = struct {
+        pub fn run(_: ParsedFlags, _: CommandFnArguments) anyerror!void {
+            return;
+        }
+    }.run;
+
+    var buffer = std.ArrayList(u8).init(std.testing.allocator);
+    defer buffer.deinit();
+
+    var writer = buffer.writer();
+    const output_stream = OutputStream.initStdErrWriter(writer.any());
+
+    var command = try Command.init("stringer", "manipulate strings", runnable, output_stream, std.testing.allocator);
+    defer command.deinit();
+
+    try command.setUsage("stringer <string>");
+    try command.addAliases(&[_]CommandAlias{ "str", "strm" });
+
+    var diagnostics: Diagnostics = .{};
+    var flags = Flags.init(std.testing.allocator);
+    defer flags.deinit();
+
+    try flags.addFlag(Flag.builder("verbose", "describe verbosity", FlagType.boolean).build(), &diagnostics);
+    try flags.addFlag(Flag.builder("priority", "describe priority", FlagType.int64).build(), &diagnostics);
+    try flags.addFlag(Flag.builder("timeout", "define timeout", FlagType.int64).build(), &diagnostics);
+    try flags.addHelp();
+
+    var command_help = CommandHelp.init(command, output_stream);
+
+    try command_help.printHelp(std.testing.allocator, &flags);
+
+    try std.testing.expect(std.mem.indexOf(u8, buffer.items, "Usage: stringer <string>").? >= 0);
     try std.testing.expect(std.mem.indexOf(u8, buffer.items, "stringer").? >= 0);
     try std.testing.expect(std.mem.indexOf(u8, buffer.items, "str").? >= 0);
     try std.testing.expect(std.mem.indexOf(u8, buffer.items, "strm").? >= 0);
