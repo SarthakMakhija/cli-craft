45183f0dd4988a329baddfef676df52563a9b39d
Sarthak <sarthak.makhija@gmail.com>
2025-07-19 09:31:40 +0530

    Sarthak | Updates the comments in build.zig

--- build.zig
+++ build.zig
@@ -1,28 +1,20 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
+// Declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+    // what target to build for.
     const target = b.standardTargetOptions(.{});
 
     // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const optimize = b.standardOptimizeOption(.{});
 
     // This creates a "module", which represents a collection of source files alongside
     // some compilation options, such as optimization mode and linked system libraries.
     // Every executable or library we compile will be based on one or more modules.
     const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/cli-craft.zig"),
         .target = target,
         .optimize = optimize,
@@ -32,7 +24,7 @@ pub fn build(b: *std.Build) void {
     const pretty_dep = b.dependency("prettytable", .{});
     const pretty_mod = pretty_dep.module("prettytable");
 
-    // Now, we will create a static library based on the module we created above.
+    // Now, create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
     // for actually invoking the compiler.
     const lib = b.addLibrary(.{
